41b766d917e65c1fd2d4d2a333f078cb4347afdd
Marco <ma.souza.junior@gmail.com>
2022-03-18 21:41:41 -0300

    enh: use ivy as fuzzy finder

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -20,11 +20,11 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       company             ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;(ivy +fuzzy)      ; a search engine for love and life
-       (vertico +fuzzy)           ; the search engine of the future
+       (ivy +fuzzy)        ; a search engine for love and life
+       ;;(vertico +fuzzy)  ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
